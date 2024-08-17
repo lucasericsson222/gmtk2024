61c42a9fd65e4f6339c2ea7abf5356c70cd8546e
Lucas Ericsson <lucasericsson222@gmail.com>
2024-08-16 17:53:55 -0700

    Doctor succesfully walking

--- scripts/doctor.gd
+++ scripts/doctor.gd
@@ -4,19 +4,27 @@ enum {RUNNING, ATTACKING}
 
 var state = RUNNING
 var speed = 200
-var max_speed = 200
+var max_speed: float = 75
+var position_fix_speed: float = 8 
 
 var target: Node2D = null
 
 func _physics_process(_delta: float) -> void:
-	velocity = Vector2.ZERO
-
 	if target == null:
 		pass
 	else:
 		var displacement = (target.position - position)
-		var direction_to_move = clamp(16*8 - displacement.length(), -max_speed, max_speed) * displacement.normalized()
-		velocity += direction_to_move
+
+		$AnimatedSprite2D.flip_h = displacement.x < 0
+
+		if displacement.length() > 6 * 16:
+			velocity += position_fix_speed * displacement.normalized()
+		if displacement.length() < 6 * 16:
+			velocity += position_fix_speed * -displacement.normalized()
+
+	velocity /= 1.1
+
+	velocity = clamp(velocity.length(), 0, max_speed) * velocity.normalized()
 	
 	move_and_slide()
 
@@ -30,4 +38,5 @@ func _on_area_2d_body_exited(body:Node2D) -> void:
 		target = null
 
 func _on_area_2d_body_entered(body:Node2D) -> void:
+	
 	target = body
