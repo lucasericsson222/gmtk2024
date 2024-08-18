4cfba5b2955cafa58bff2891173f811ffa79195a
Lucas Ericsson <lucasericsson222@gmail.com>
2024-08-18 12:12:50 -0700

    Zombie throwing working; minor animation bug

--- scripts/super_zombie.gd
+++ scripts/super_zombie.gd
@@ -1,13 +1,50 @@
 extends CharacterBody2D
 
 @onready var zombie_component = $ZombieComponent
-enum {NORMAL, ASCENDING, DYING}
+enum {NORMAL, ASCENDING, DYING, THROWING}
 var state = NORMAL
 
-func _physics_process(_delta: float) -> void:
-	# Add the gravity.
+var grab_for_throw_dist = 16
 
-	velocity = zombie_component.velocity
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("throw"):
+		state = THROWING
+
+var target = null
+
+func _draw() -> void:
+	# enable queue_redraw() in physics_process and uncomment to see
+	#if target != null:
+		#draw_line(Vector2.ZERO, target.position - position, Color.WHITE, 1)
+	pass
+	
+func _physics_process(_delta: float) -> void:
+	#queue_redraw()
+	# Add the gravity.
+	if state == NORMAL:
+		velocity = zombie_component.velocity
+	if state == THROWING:
+		var zombies = get_tree().get_nodes_in_group("throwable_zombie")
+		if target == null:
+			if zombies.size() > 0:
+				var closest = zombies[0]
+				var min_dist = INF
+				for zombie: Node2D in zombies:
+					if zombie.get_instance_id() == get_instance_id():
+						continue
+					var dist = (zombie.position - position).length()
+					if dist < min_dist:
+						closest = zombie
+						min_dist = dist
+				target = closest
+		else:
+			velocity = zombie_component.max_speed * (target.position - position).normalized()
+		var dist = (target.position - position).length()
+		if dist < grab_for_throw_dist:
+			target.be_thrown((get_global_mouse_position() - position).angle())
+			state = NORMAL
+			target = null
+			
 	if $AnimatedSprite2D.animation == "death":
 		velocity = Vector2.ZERO
 
--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -1,24 +1,45 @@
 extends CharacterBody2D
 
 @onready var zombie_component = $ZombieComponent
-enum {NORMAL, ASCENDING, DYING}
+enum {NORMAL, ASCENDING, DYING, THROWN}
 var state = NORMAL
 var max_id = 0
 
+var thrown_speed = 200
+
 var can_ascend = false
-
-func _physics_process(_delta: float) -> void:
+var z_throw_velocity = 2
+var z_velocity = 0
+var z_gravity = -5
+var z_pos = 0
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
-
-	velocity = zombie_component.velocity
+	
+	if state == NORMAL:
+		velocity = zombie_component.velocity
 	if $AnimatedSprite2D.animation == "death":
 		velocity = Vector2.ZERO
+	if state == THROWN:
+		z_velocity += z_gravity * delta
+		z_pos += z_velocity
+		if z_pos <= 0:
+			z_pos = 0
+			z_velocity = 0
+			state = NORMAL
+			$AnimatedSprite2D.animation = "default"
+		$AnimatedSprite2D.position.y = -z_pos
 
 	$AnimatedSprite2D.flip_h = velocity.x < 0
 	
 	if state == ASCENDING:
 		return
 	move_and_slide()
+	
+func be_thrown(dir: float):
+	state = THROWN
+	velocity = thrown_speed * Vector2.RIGHT.rotated(dir)
+	$AnimatedSprite2D.animation = "thrown"
+	z_velocity = z_throw_velocity
 
 func ascend():
 	state = ASCENDING
