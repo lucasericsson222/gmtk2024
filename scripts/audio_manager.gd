7958783c3d8abe85647eecf2e744e591ea490100
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2024-08-19 18:23:54 -0700

    sfx and resume button

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@onready var sfxStreams = $SFXStreams
+
 enum SoundEffects {
 	MENU_BUTTON,
 	ZOMBIE_GROAN_1,
-	ZOMBIE_GROAN_2
+	ZOMBIE_GROAN_2,
+	TABLE_BREAK,
 }
 
 enum Songs {
@@ -16,7 +19,8 @@ enum Songs {
 const SFX_RESOURCES := {
 	SoundEffects.MENU_BUTTON: preload("res://audio/menu_select.wav"),
 	SoundEffects.ZOMBIE_GROAN_1: preload("res://audio/zombie_1.wav"),
-	SoundEffects.ZOMBIE_GROAN_2: preload("res://audio/zombie_2.wav")
+	SoundEffects.ZOMBIE_GROAN_2: preload("res://audio/zombie_2.wav"),
+	SoundEffects.TABLE_BREAK: preload("res://audio/table_break.wav")
 }
 
 const SONG_RESOURCES := {
@@ -26,9 +30,13 @@ const SONG_RESOURCES := {
 	Songs.WIN: preload("res://audio/win.mp3")
 }
 
+
 func play_sfx(sfx: SoundEffects):
-	$SFX.stream = SFX_RESOURCES[sfx]
-	$SFX.play()
+	for audioStreamPlayer in sfxStreams.get_children():
+		if not audioStreamPlayer.is_playing():
+			audioStreamPlayer.stream = SFX_RESOURCES[sfx]
+			audioStreamPlayer.play()
+			break
 
 func play_song(song: Songs):
 	$Music.stop()
--- scripts/table.gd
+++ scripts/table.gd
@@ -16,5 +16,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("throwable_zombie"):
 		return
 	animation = "broken"
+	AudioManager.play_sfx(AudioManager.SoundEffects.TABLE_BREAK)
 	$Area2D/CollisionShape2D.set_deferred("disabled", true)
 	$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
